d0f2b952f03a919f28d7bea90b6673969c6f8858
Sihwan Kim <vkn123@naver.com>
2024-11-03 22:30:47 +0900

    feat: android 이미지 뷰어 기능 구현

--- PhotoViewer/app/src/main/java/com/van1164/photoviewer/MainActivity.java
+++ PhotoViewer/app/src/main/java/com/van1164/photoviewer/MainActivity.java
@@ -1,24 +1,127 @@
 package com.van1164.photoviewer;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.van1164.photoviewer.imageview.ImageAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+    ImageView imgView;
+    TextView textView;
+    String site_url = "http://10.0.2.2:8000";
+    JSONObject post_json;
+    String imageUrl = null;
+    Bitmap bmImg = null;
+    CloadImage taskDownload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //imgView = (ImageView) findViewById(R.id.imgView);
+        textView = (TextView) findViewById(R.id.textView);
+
+    }
+
+    public void onClickDownload(View v) {
+//...생략...
+        if (taskDownload != null && taskDownload.getStatus() == AsyncTask.Status.RUNNING) {
+            taskDownload.cancel(true);
+        }
+        taskDownload = new CloadImage();
+        taskDownload.execute(site_url + "/api_root/Post/");
+        Toast.makeText(getApplicationContext(), "Download", Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickUpload(View v) {
+//...여기에 코드 추가...
+        Toast.makeText(getApplicationContext(), "Upload", Toast.LENGTH_LONG).show();
+    }
+
+    private class CloadImage extends AsyncTask<String, Integer, List<Bitmap>> {
+        @Override
+        protected List<Bitmap> doInBackground(String... urls) {
+            List<Bitmap> bitmapList = new ArrayList<>();
+            try {
+                String apiUrl = urls[0];
+                String token = "462ee69e53a4851f9480f4061d8999018d0d305c";
+                URL urlAPI = new URL(apiUrl);
+                HttpURLConnection conn = (HttpURLConnection) urlAPI.openConnection();
+                conn.setRequestProperty("Authorization", "Token " + token);
+                conn.setRequestMethod("GET");
+                conn.setConnectTimeout(3000);
+                conn.setReadTimeout(3000);
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    InputStream is = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+                    is.close();
+
+                    String strJson = result.toString();
+                    JSONArray aryJson = new JSONArray(strJson);
+// 배열 내 모든 이미지 다운로드
+                    for (int i = 0; i < aryJson.length(); i++) {
+                        post_json = (JSONObject) aryJson.get(i);
+                        imageUrl = post_json.getString("image");
+                        if (!imageUrl.equals("")) {
+                            URL myImageUrl = new URL(imageUrl);
+                            conn = (HttpURLConnection) myImageUrl.openConnection();
+                            InputStream imgStream = conn.getInputStream();
+                            Bitmap imageBitmap = BitmapFactory.decodeStream(imgStream);
+                            bitmapList.add(imageBitmap); // 이미지 리스트에 추가
+                            imgStream.close();
+                        }
+                    }
+                }
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+            return bitmapList;
+        }
+
+        @Override
+        protected void onPostExecute(List<Bitmap> images) {
+            if (images.isEmpty()) {
+                textView.setText("불러올 이미지가 없습니다.");
+            } else {
+                textView.setText("이미지 로드 성공!");
+                RecyclerView recyclerView = findViewById(R.id.recyclerView);
+                ImageAdapter adapter = new ImageAdapter(images);
+                recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                recyclerView.setAdapter(adapter);
+            }
+        }
     }
 }
